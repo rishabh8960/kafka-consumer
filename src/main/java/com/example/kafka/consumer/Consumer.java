22b06352ecd8d963676e1d1fbf0b87ec052a5d4d
Rishabh <rishabhchaurasiya007@gmail.com>
2025-05-23 00:26:42 +0530

    added read me

--- src/main/java/com/example/kafka/consumer/Consumer.java
+++
@@ -1,9 +0,0 @@
-package com.example.kafka.consumer;
-
-//@Service
-//public class Consumer {
-//    @KafkaListener(topics = "Uniper_Topic", groupId = "demo")
-//    public void consume(String message) {
-//        System.out.println("Consumed message: " + message);
-//    }
-//}
